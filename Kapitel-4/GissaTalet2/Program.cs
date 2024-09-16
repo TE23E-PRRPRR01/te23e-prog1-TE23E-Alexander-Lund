a01f52c86f27b695b794e4c4beca557b5c788e1c
Alexander Lund <alexander.lund@elev.ga.ntig.se>
2024-09-16 14:43:28 +0200

    Tränat loop med while true

---
+++ Kapitel-4/GissaTalet2/Program.cs
@@ -0,0 +1,59 @@
+﻿// Spelet  - gissa ett slumptal
+Console.Clear();
+Console.WriteLine("Spelet - gissa ett slumptal 1-100");
+
+// Antal gissningar
+
+int antal = 0;
+
+// Ange intervaller
+
+Console.Write("Ange min värde: ");
+int min = int.Parse(Console.ReadLine());
+
+Console.Write("Ange max värde: ");
+int max = int.Parse(Console.ReadLine());
+
+
+
+// slumpar ett tal 1-100
+int slumptal = Random.Shared.Next(min, max);
+
+// upprepning - loop
+while (true)
+{
+
+    // ställ fråga till användaren
+    Console.Write($"Gissa ett tal ({min} - {max}): ");
+    int gissning = int.Parse(Console.ReadLine());
+
+    // Räkna upp antal med 1
+    antal++;
+
+
+    // kontroll om gissning är rätt?
+    if (gissning == slumptal)
+    {
+        Console.WriteLine($"Rätt gissat! ({antal} gissningar)");
+
+        Console.Write("Vill du spela igen? (j/n) ");
+        string spela = Console.ReadLine();
+        if (spela == "n")
+        {
+            break;
+        }
+    }
+    else if (gissning > slumptal)
+    {
+        Console.WriteLine("För högt!");
+    }
+    else
+    {
+        Console.WriteLine("För lågt!");
+    }
+
+
+}
+
+//slut på spelet
+Console.WriteLine("Tack för att du spelade!");
