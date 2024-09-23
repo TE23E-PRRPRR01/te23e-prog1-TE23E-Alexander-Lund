594ef2cae342836dc1f9cd0567e3c0d1257c4931
Alexander Lund <alexander.lund@elev.ga.ntig.se>
2024-09-23 14:08:09 +0200

    Meny övning med while loop

---
+++ Kapitel-4/MenyProgram/Program.cs
@@ -0,0 +1,49 @@
+﻿// MenyProgram
+Console.Clear();
+Console.WriteLine("Litet program med meny");
+Console.InputEncoding = System.Text.Encoding.Unicode;
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+// Variabel för svaren
+
+string val = "";
+
+// programloop
+
+while (true)
+{
+    // Visar meny och läser användarens val
+    Console.Write("""
+
+    1. Omvandla en text till versaler
+
+    2. Omvandla en text till gemener
+
+    3. avsluta
+
+    Välj ett av alternativen ovan: 
+    """);
+    val = Console.ReadLine();
+    
+    // hantera användarens val
+    if (val == "1")
+    {
+        Console.Write("Skriv in en text; ");
+        string text = Console.ReadLine().ToUpper();
+        Console.WriteLine($"Texten i versaler blir; {text}");
+    }
+    else if (val == "2")
+    {
+        Console.Write("Skriv in en text; ");
+        string text2 = Console.ReadLine().ToLower();
+        Console.WriteLine($"Texten i gemener blir; {text2}");
+    }
+    else if (val == "3")
+    {
+        Console.WriteLine("Avslutar..");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Ogiltig input, försök igen");
+    }
+}
