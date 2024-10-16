ace5d23c6aefc38a48bd10c2b9f56f438ee6f00e
Alexander Lund <alexander.lund@elev.ga.ntig.se>
2024-10-16 14:14:11 +0200

    morseprogram, övn på list (WIP)

---
+++ Kapitel-5/Morsekod/Program.cs
@@ -0,0 +1,34 @@
+﻿// morse kod program
+Console.WriteLine("Översättningsprogram för morsekod.");
+Console.Clear();
+
+// två listor
+List<string> alfabetet = [  "A", "B", "C", "D", "E", "F", "G", "H", "I", 
+                            "J", "K", "L", "M", "N", "O", "P", "Q", "R", 
+                            "S", "T", "U", "V", "W", "X", "Y", "Z", "Å", "Ä", "Ö"];
+
+List<string> morsekod = [    ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", 
+                             "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", 
+                             "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", 
+                              "-.--", "--..", ".--.-", ".-.-", "---.", "/" ];
+
+// Läs in en text
+
+Console.Write("Ange ett meddelande: ");
+string bokstav = Console.ReadLine();
+
+// uppslag i alfabetet efter index
+int index = alfabetet.IndexOf(bokstav);
+
+if (index >= 0)
+{
+    Console.WriteLine($"{bokstav} finns på index {index}");
+
+    // plocka ut morsetecken för detta index
+    string morsetecken = morsekod[index];
+    Console.WriteLine($"{bokstav} är i morsekod {morsetecken}");
+}
+else
+{
+    Console.WriteLine("error");
+}
